eadc12e36a004d419a12c873a7990c84f856e769
Mark Njoroge <marknjo7@gmail.com>
2020-01-10 16:13:55 +0300

    add a mesh object to the scene (red box)

--- 03.basic-scene/script.js
+++ 03.basic-scene/script.js
@@ -3,3 +3,9 @@ const canvasEl = document.querySelector('.webgl');
 
 // 1). Add A Scene
 const scene = new THREE.Scene();
+
+// 2). Add a mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: '#ff0000' }); // red
+const mesh = new THREE.Mesh(geometry);
+scene.add(mesh);
