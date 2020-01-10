66e46aceb92ec823e3f6c2c83c4414ed0b7e445d
Mark Njoroge <marknjo7@gmail.com>
2020-01-10 16:20:26 +0300

    render a red box to the canvas

--- 03.basic-scene/script.js
+++ 03.basic-scene/script.js
@@ -7,7 +7,7 @@ const scene = new THREE.Scene();
 // 2). Add a mesh
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: '#ff0000' }); // red
-const mesh = new THREE.Mesh(geometry);
+const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 // Sizes
@@ -19,3 +19,13 @@ const sizes = {
 // 4). Add A Camera to the scene
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
+
+scene.add(camera);
+
+// 5). Add renderer to the scene
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvasEl,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
