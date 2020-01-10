5741175d1c83dd6733577e9e14445ca5e48a1350
Mark Njoroge <marknjo7@gmail.com>
2020-01-10 16:16:47 +0300

    add a perspective camera to the scene

--- 03.basic-scene/script.js
+++ 03.basic-scene/script.js
@@ -9,3 +9,13 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: '#ff0000' }); // red
 const mesh = new THREE.Mesh(geometry);
 scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// 4). Add A Camera to the scene
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
