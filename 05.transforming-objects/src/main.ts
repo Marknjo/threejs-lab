b639a44f503b29da48be3b464c5d64da610503c3
Mark Njoroge <marknjo7@gmail.com>
2020-01-15 16:57:25 +0300

    positioning mesh xyz - vector3

--- 05.transforming-objects/src/main.ts
+++ 05.transforming-objects/src/main.ts
@@ -11,6 +11,10 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: '#ff0000' });
 const mesh = new THREE.Mesh(geometry, material);
 
+mesh.position.x = 0.7;
+mesh.position.y = -0.6;
+mesh.position.z = 1;
+
 scene.add(mesh);
 
 // Sizes
