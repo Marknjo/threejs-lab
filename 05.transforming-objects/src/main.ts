1367137cc6e9edaec9109bd425e31aac5bf90685
Mark Njoroge <marknjo7@gmail.com>
2020-01-17 17:25:15 +0300

    grouping meshes

--- 05.transforming-objects/src/main.ts
+++ 05.transforming-objects/src/main.ts
@@ -6,12 +6,46 @@ const canvasEl = document.querySelector('.webgl')! as HTMLCanvasElement;
 // Scene
 const scene = new THREE.Scene();
 
-// Mesh
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' });
-const mesh = new THREE.Mesh(geometry, material);
+// Objects
+const group = new THREE.Group();
+scene.add(group);
 
-scene.add(mesh);
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+// Meshes
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+
+group.add(cube3);
+
+scene.add(group);
+
+cube1.position.x = 2;
+cube3.position.x = -2;
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: '#ff0000' });
+// const mesh = new THREE.Mesh(geometry, material);
+
+// scene.add(mesh);
 
 /// -> Positioning
 // mesh.position.normalize();
@@ -20,22 +54,18 @@ scene.add(mesh);
 // mesh.position.x = 0.7;
 // mesh.position.y = -0.6;
 // mesh.position.z = -2;
-mesh.position.set(0.7, -0.6, 1);
+// mesh.position.set(0.7, -0.6, 1);
 
 /// -> Scaling
 // mesh.scale.x = 2;
 // mesh.scale.y = 0.5;
 // mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 0.75);
+// mesh.scale.set(2, 0.5, 0.75);
 
 /// -> Rotation
-mesh.rotation.reorder('YXZ');
-mesh.rotation.y = Math.PI * 0.75;
-mesh.rotation.x = Math.PI * 0.25;
-
-// Axes Helper
-const axesHelper = new THREE.AxesHelper(3);
-scene.add(axesHelper);
+// mesh.rotation.reorder('YXZ');
+// mesh.rotation.y = Math.PI * 0.75;
+// mesh.rotation.x = Math.PI * 0.25;
 
 // Sizes
 const sizes = {
@@ -51,9 +81,9 @@ camera.position.x = 0.5;
 
 scene.add(camera);
 
-camera.lookAt(mesh.position);
+camera.lookAt(cube2.position);
 
-console.log(mesh.position.distanceTo(camera.position));
+// console.log(cube1.position.distanceTo(camera.position));
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
