8011907df25c0c39953d31e568acb1c1c8e08503
Mark Njoroge <marknjo7@gmail.com>
2020-01-27 09:35:37 +0300

    toggling wireframe mode on gemetries

--- 09.geometries/src/main.ts
+++ 09.geometries/src/main.ts
@@ -8,8 +8,11 @@ const canvasEl = document.querySelector('.webgl')! as HTMLCanvasElement;
 const scene = new THREE.Scene();
 
 // Mesh
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' });
+const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+const material = new THREE.MeshBasicMaterial({
+  color: '#ff0000',
+  wireframe: true,
+});
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
