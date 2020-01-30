b15bb1f4a9401679fa33c265e43c970bbf988e8f
Mark Njoroge <marknjo7@gmail.com>
2020-01-30 10:14:53 +0300

    setup debugging ui lesson

---
+++ 10.debug-ui/src/main.ts
@@ -0,0 +1,85 @@
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const canvasEl = document.querySelector('.webgl')! as HTMLCanvasElement;
+
+// Scene
+const scene = new THREE.Scene();
+
+// Mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: '#ff0000' });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+
+scene.add(camera);
+
+// Handle auto resize of the canvas in the viewport
+window.addEventListener('resize', () => {
+  // update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// handle fullscreen
+window.addEventListener('dblclick', () => {
+  if (!document.fullscreenElement) {
+    canvasEl.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});
+
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, canvasEl);
+// controls.enabled = true;
+controls.enableDamping = true;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvasEl,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animation
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // update controls
+  controls.update();
+
+  // Renderer
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
