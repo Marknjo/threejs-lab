b5fbb09e9212a910cb157f3b64ff7b9095fd443c
Mark Njoroge <marknjo7@gmail.com>
2020-02-01 10:44:02 +0300

    add basic lil-gui debug settings to the cube

--- 10.debug-ui/src/main.ts
+++ 10.debug-ui/src/main.ts
@@ -1,85 +1,117 @@
-import './style.css';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import GUI from 'lil-gui'
 
-const canvasEl = document.querySelector('.webgl')! as HTMLCanvasElement;
+/**
+ * Canvas
+ */
+const canvasEl = document.querySelector(
+  '.webgl'
+)! as HTMLCanvasElement
+
+/**
+ * Debug
+ */
+const gui = new GUI()
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 // Mesh
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' });
-const mesh = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({
+  color: '#ff0000',
+})
+const mesh = new THREE.Mesh(geometry, material)
 
-scene.add(mesh);
+scene.add(mesh)
+
+// Debug
+gui
+  .add(mesh.position, 'y')
+  .min(-3)
+  .max(3)
+  .step(0.01)
+  .name('Elevation')
+
+gui.add(mesh, 'visible').name('Toggle Visibility')
+
+gui.add(material, 'wireframe').name('Show Wireframe')
+
+gui.addColor(material, 'color').name('Name')
 
 // Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
-};
+}
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height
+)
+camera.position.z = 3
 
-scene.add(camera);
+scene.add(camera)
 
 // Handle auto resize of the canvas in the viewport
 window.addEventListener('resize', () => {
   // update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
   // update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
   // update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(
+    Math.min(window.devicePixelRatio, 2)
+  )
+})
 
 // handle fullscreen
 window.addEventListener('dblclick', () => {
   if (!document.fullscreenElement) {
-    canvasEl.requestFullscreen();
+    canvasEl.requestFullscreen()
   } else {
-    document.exitFullscreen();
+    document.exitFullscreen()
   }
-});
+})
 
 /**
  * Controls
  */
-const controls = new OrbitControls(camera, canvasEl);
+const controls = new OrbitControls(camera, canvasEl)
 // controls.enabled = true;
-controls.enableDamping = true;
+controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvasEl,
-});
+})
 
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animation
  */
-const clock = new THREE.Clock();
+const clock = new THREE.Clock()
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+  const elapsedTime = clock.getElapsedTime()
 
   // update controls
-  controls.update();
+  controls.update()
 
   // Renderer
-  renderer.render(scene, camera);
+  renderer.render(scene, camera)
 
-  window.requestAnimationFrame(tick);
-};
+  window.requestAnimationFrame(tick)
+}
 
-tick();
+tick()
