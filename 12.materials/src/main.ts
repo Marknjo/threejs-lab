ac45aa105057127c5a9f359dd2be9681dfa730a6
Mark Njoroge <marknjo7@gmail.com>
2020-02-06 12:05:15 +0300

    (L12) add to the scene torus|sphere|plane meshes

--- 12.materials/src/main.ts
+++ 12.materials/src/main.ts
@@ -1,88 +1,128 @@
-import './style.css';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 /**
  * Canvas
  */
-const canvasEl = document.querySelector('.webgl')! as HTMLCanvasElement;
+const canvasEl = document.querySelector(
+  '.webgl'
+)! as HTMLCanvasElement
+
+/**
+ *
+ */
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
-// Mesh
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' });
-const mesh = new THREE.Mesh(geometry, material);
+/**
+ * Meshes
+ */
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+})
 
-scene.add(mesh);
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  material
+)
+
+sphere.position.x = -1.5
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(1, 1),
+  material
+)
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+  material
+)
+torus.position.x = 1.5
+
+scene.add(sphere, plane, torus)
 
 // Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
-};
+}
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height
+)
+camera.position.z = 3
 
-scene.add(camera);
+scene.add(camera)
 
 // Handle auto resize of the canvas in the viewport
 window.addEventListener('resize', () => {
   // update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
   // update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
   // update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(
+    Math.min(window.devicePixelRatio, 2)
+  )
+})
 
 // handle fullscreen
 window.addEventListener('dblclick', () => {
   if (!document.fullscreenElement) {
-    canvasEl.requestFullscreen();
+    canvasEl.requestFullscreen()
   } else {
-    document.exitFullscreen();
+    document.exitFullscreen()
   }
-});
+})
 
 /**
  * Controls
  */
-const controls = new OrbitControls(camera, canvasEl);
+const controls = new OrbitControls(camera, canvasEl)
 // controls.enabled = true;
-controls.enableDamping = true;
+controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvasEl,
-});
+})
 
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animation
  */
-const clock = new THREE.Clock();
+const clock = new THREE.Clock()
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+  const elapsedTime = clock.getElapsedTime()
+
+  // Update objects
+  sphere.rotation.y = 0.1 * elapsedTime
+  plane.rotation.y = 0.1 * elapsedTime
+  torus.rotation.y = 0.1 * elapsedTime
+
+  sphere.rotation.x = 0.1 * elapsedTime
+  plane.rotation.x = 0.1 * elapsedTime
+  torus.rotation.x = 0.1 * elapsedTime
 
   // update controls
-  controls.update();
+  controls.update()
 
   // Renderer
-  renderer.render(scene, camera);
+  renderer.render(scene, camera)
 
-  window.requestAnimationFrame(tick);
-};
+  window.requestAnimationFrame(tick)
+}
 
-tick();
+tick()
