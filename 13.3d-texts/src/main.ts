f4f85fd4016fb1923a1564aa3a2430718ef63eab
Mark Njoroge <marknjo7@gmail.com>
2020-02-11 14:39:18 +0300

    (L13) load project text to the project

--- 13.3d-texts/src/main.ts
+++ 13.3d-texts/src/main.ts
@@ -1,88 +1,115 @@
-import './style.css';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { FontLoader } from 'three/addons/loaders/FontLoader.js'
 
 /**
  * Canvas
  */
-const canvasEl = document.querySelector('.webgl')! as HTMLCanvasElement;
+const canvasEl = document.querySelector(
+  '.webgl'
+)! as HTMLCanvasElement
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
+
+/**
+ * Fonts
+ */
+const fontLoadingManager = new THREE.LoadingManager()
+
+fontLoadingManager.onLoad = () => {
+  console.log('Font loaded')
+}
+
+fontLoadingManager.onError = () => {
+  console.error('Error loading font 💥💥💥')
+}
+
+const fontLoader = new FontLoader(fontLoadingManager)
+
+fontLoader.load('/fonts/helvetiker_regular.typeface.json')
 
 // Mesh
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' });
-const mesh = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({
+  color: '#ff0000',
+})
+const mesh = new THREE.Mesh(geometry, material)
 
-scene.add(mesh);
+scene.add(mesh)
 
 // Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
-};
+}
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height
+)
+camera.position.z = 3
 
-scene.add(camera);
+scene.add(camera)
 
 // Handle auto resize of the canvas in the viewport
 window.addEventListener('resize', () => {
   // update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
   // update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
   // update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(
+    Math.min(window.devicePixelRatio, 2)
+  )
+})
 
 // handle fullscreen
 window.addEventListener('dblclick', () => {
   if (!document.fullscreenElement) {
-    canvasEl.requestFullscreen();
+    canvasEl.requestFullscreen()
   } else {
-    document.exitFullscreen();
+    document.exitFullscreen()
   }
-});
+})
 
 /**
  * Controls
  */
-const controls = new OrbitControls(camera, canvasEl);
+const controls = new OrbitControls(camera, canvasEl)
 // controls.enabled = true;
-controls.enableDamping = true;
+controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvasEl,
-});
+})
 
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animation
  */
-const clock = new THREE.Clock();
+const clock = new THREE.Clock()
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+  const elapsedTime = clock.getElapsedTime()
 
   // update controls
-  controls.update();
+  controls.update()
 
   // Renderer
-  renderer.render(scene, camera);
+  renderer.render(scene, camera)
 
-  window.requestAnimationFrame(tick);
-};
+  window.requestAnimationFrame(tick)
+}
 
-tick();
+tick()
